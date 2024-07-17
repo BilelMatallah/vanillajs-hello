d013cb0f5495d7e5e62d22bfbe2234892ce9838c
BilelMatallah <107211669+BilelMatallah@users.noreply.github.com>
2024-07-17 10:41:29 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = [
+  "The dog ",
+  "My grandma ",
+  "The mailman ",
+  "My bird ",
+  "The president "
+];
+let action = ["ate ", "peed ", "crushed ", "broke ", "burned "];
+let what = ["my homework ", "my phone ", "the car ", "my house "];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+let part1 = Math.floor(Math.random() * who.length);
+let part2 = Math.floor(Math.random() * action.length);
+let part3 = Math.floor(Math.random() * what.length);
+let part4 = Math.floor(Math.random() * when.length);
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#excuse").innerHTML =
+    who[part1] + action[part2] + what[part3] + when[part4];
 };
